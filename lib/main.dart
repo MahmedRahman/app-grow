5e24a5afd788d1be327857c31c26fb9490045dca
Mohamed Rahman <atpfreelancer@gmail.com>
2021-06-06 15:15:24 +0200

    updater

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:grow/app/data/app_constand.dart';
 import 'app/routes/app_pages.dart';
 
 void main() async {
-  
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
@@ -21,7 +20,7 @@ void main() async {
         scaffoldBackgroundColor: KaccentColor,
         fontFamily: 'Cairo',
       ),
-      builder: EasyLoading.init() ,
+      builder: EasyLoading.init(),
     ),
   );
 }
